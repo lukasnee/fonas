735a125ffc6f010e26bf05d730c6811b6b1a10c3
lukasnee <lukas.neverauskis@gmail.com>
2026-02-16 20:15:45 +0200

    feat: ln::core::shell_cmds: reset, test_fault, uptime

---
+++ ln/core/src/ln_shell_cmds.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2026 Lukas Neverauskis https://github.com/lukasnee
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include "ln/shell/CLI.hpp"
+#include "ln/ln.h"
+
+namespace ln::shell {
+
+Cmd reset_cmd{Cmd::Cfg{.name = "reset",
+                       .short_description = "soft system reset",
+                       .fn = []([[maybe_unused]]
+                                Cmd::Ctx ctx) {
+                           ln::reset();
+                           return Err::unexpected;
+                       }}};
+
+Cmd panic_cmd{Cmd::Cfg{.name = "test_fault",
+                       .short_description = "test fault behavior (handling)",
+                       .long_description = R"(Usage:
+  test_fault panic
+  test_fault read_invalid_address
+
+Commands:
+  panic                  test panic behavior
+  read_invalid_address   test hardfault behavior by reading from an invalid address
+)",
+                       .fn = [](Cmd::Ctx ctx) {
+                           if (ctx.args.size() != 1) {
+                               return Err::badArg;
+                           }
+                           using namespace std::literals::string_view_literals;
+                           if (ctx.args[0] == "panic"sv) {
+                               LN_PANIC_WITH_MSG("this is a test");
+                           }
+                           else if (ctx.args.size() == 1 &&
+                                    ctx.args[0] == "read_invalid_address"sv) {
+                               const auto invalid_address = 0xbadcafe;
+                               return *reinterpret_cast<Err *>(invalid_address);
+                           }
+                           ctx.cli.printf("unknown subcommand '%.*s'\n",
+                                          static_cast<int>(ctx.args[0].size()),
+                                          ctx.args[0].data());
+                           return Err::badArg;
+                       }}};
+
+Cmd uptime_cmd{
+    Cmd::Cfg{.name = "uptime",
+             .short_description = "show system uptime in milliseconds",
+             .fn = [](Cmd::Ctx ctx) {
+                 ctx.cli.printf("%lu\n", ln::get_uptime_ms().count());
+                 return Err::ok;
+             }}};
+
+} // namespace ln::shell
---
+++ port/stdcpp/src/port.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2026 Lukas Neverauskis https://github.com/lukasnee
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <chrono>
+#include <cstdlib>
+#include <cstdio>
+
+namespace ln {
+
+void panic(const char *file, int line, const char *message) {
+    printf("\n%s:%d: panic%s%s\n", file, line, (message ? ": " : "!"),
+           (message ? message : ""));
+    std::abort();
+}
+
+void reset() { std::abort(); }
+
+const auto g_start_time = std::chrono::steady_clock::now();
+
+std::chrono::milliseconds get_uptime_ms() {
+    const auto now = std::chrono::steady_clock::now();
+    const auto elapsed = now - g_start_time;
+    return std::chrono::duration_cast<std::chrono::milliseconds>(elapsed);
+}
+
+} // namespace ln
