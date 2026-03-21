ca5390c0c340cead30c0322adca11964affb17ac
lukasnee <lukas.neverauskis@gmail.com>
2026-03-21 19:30:01 +0200

    refactor: fmt based print_run_time_stats (FreeRTOS)
    configUSE_STATS_FORMATTING_FUNCTIONS maybe set to 0 in FreeRTOSConfig.h

--- port/FreeRTOS/src/port_shell_cmds.cpp
+++ port/FreeRTOS/src/port_shell_cmds.cpp
@@ -2,23 +2,51 @@
 
 #include "FreeRTOS/Addons/Clock.hpp"
 
+void print_run_time_stats(ln::shell::CLI &cli) {
+    auto uxCurrentNumberOfTasks = uxTaskGetNumberOfTasks();
+    UBaseType_t uxArraySize = uxCurrentNumberOfTasks;
+    TaskStatus_t *pxTaskStatusArray = static_cast<TaskStatus_t *>(
+        pvPortMalloc(uxCurrentNumberOfTasks * sizeof(TaskStatus_t)));
+    if (!pxTaskStatusArray) {
+        return;
+    }
+    uint32_t ulTotalTime = 0UL;
+    uxArraySize =
+        uxTaskGetSystemState(pxTaskStatusArray, uxArraySize, &ulTotalTime);
+    ulTotalTime /= 100UL;
+    if (ulTotalTime == 0) {
+        return;
+    }
+    std::string_view line_fmt = "{:<16}{:<12}{:<12}\n";
+    cli.print(line_fmt, "Task", "time,.1ms", "time,%");
+    for (UBaseType_t x = 0; x < uxArraySize; x++) {
+        uint32_t ulStatsAsPercentage =
+            pxTaskStatusArray[x].ulRunTimeCounter / ulTotalTime;
+        if (ulStatsAsPercentage > 0UL) {
+            cli.print(line_fmt, pxTaskStatusArray[x].pcTaskName,
+                      pxTaskStatusArray[x].ulRunTimeCounter,
+                      ulStatsAsPercentage);
+        }
+        else {
+            cli.print(line_fmt, pxTaskStatusArray[x].pcTaskName,
+                      pxTaskStatusArray[x].ulRunTimeCounter, "<1%");
+        }
+    }
+    vPortFree(pxTaskStatusArray);
+}
+
 namespace ln::shell {
 
-Cmd status_cmd{Cmd::Cfg{
-    .name = "status,s",
-    .short_description = "show system status",
-    .fn = [](Cmd::Ctx ctx) {
-        const size_t buff_size_for_one_task = 20;
-        const size_t max_tasks = 40;
-        const size_t buff_size = buff_size_for_one_task * max_tasks;
-        char run_time_stats_buf[buff_size];
-        vTaskGetRunTimeStats(run_time_stats_buf);
-        ctx.cli.print("uptime: {}\n", ln::get_uptime_ms());
-        ctx.cli.print("freeHeapSize: {}\n\r", xPortGetFreeHeapSize());
-        ctx.cli.print("RunTimeStatsCntVal: {}\n",
-                      uint32GetRunTimeCounterValue());
-        ctx.cli.print("Task\t\ttime,.1ms\ttime,%\n{}", run_time_stats_buf);
-        return Err::ok;
-    }}};
+Cmd status_cmd{Cmd::Cfg{.name = "status,s",
+                        .short_description = "show system status",
+                        .fn = [](Cmd::Ctx ctx) {
+                            ctx.cli.print("uptime: {}\n", ln::get_uptime_ms());
+                            ctx.cli.print("freeHeapSize: {}\n\r",
+                                          xPortGetFreeHeapSize());
+                            ctx.cli.print("RunTimeStatsCntVal: {}\n",
+                                          uint32GetRunTimeCounterValue());
+                            print_run_time_stats(ctx.cli);
+                            return Err::ok;
+                        }}};
 
 } // namespace ln::shell
