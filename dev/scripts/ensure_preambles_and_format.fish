127750dc0ecdafe7de798e77739b7d0fb8d60931
lukasnee <lukas.neverauskis@gmail.com>
2026-05-03 13:18:17 +0300

    chore: ensure_preambles_and_format.fish

--- dev/scripts/ensure_preambles_and_format.fish
+++ dev/scripts/ensure_preambles_and_format.fish
@@ -3,14 +3,19 @@
 # Script to add GPL license preambles to C/C++ source files
 
 set current_year (date +%Y)
-set license_header "// Copyright (c)  $current_year Lukas Neverauskis <lukas.neverauskis@gmail.com>
+set license_header "// Copyright (c) $current_year Lukas Neverauskis <lukas.neverauskis@gmail.com>
 // SPDX-License-Identifier: GPL-2.0-only
 "
 
 set pragma_once_header "#pragma once
  "
 
-for file in (find ln/ port/ cmake/ -type f \( -name "*.h" -o -name "*.hpp" -o -name "*.c" -o -name "*.cpp" \))
+set search_paths $argv
+if test (count $search_paths) -eq 0
+    set search_paths ln/ port/ cmake/
+end
+
+for file in (find $search_paths -type f \( -name "*.h" -o -name "*.hpp" -o -name "*.c" -o -name "*.cpp" \) ! -name "FreeRTOSConfig.h")
     set temp_file (mktemp)
     if not grep -qE "[cC]opyright\s+\([cC]\)" "$file"
     echo "+preable $file"
@@ -23,4 +28,5 @@ for file in (find ln/ port/ cmake/ -type f \( -name "*.h" -o -name "*.hpp" -o -n
     end
     cat "$file" >> "$temp_file"
     mv "$temp_file" "$file"
+    clang-format -i "$file"
 end
